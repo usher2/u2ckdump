96d45559c80922c1e440e44469df6897423d13b6
Phil Kulin <schors@gmail.com>
2019-11-20 00:17:30 +0300

    separate file for ArrayIntSet

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/yl2chen/cidranger v0.0.0-20190806234802-fed7223fd934
-	golang.org/x/net v0.0.0-20191112182307-2180aed22343
-	golang.org/x/sys v0.0.0-20191113154946-fee6b63275c8 // indirect
+	golang.org/x/net v0.0.0-20191119073136-fc4aabc6c914
+	golang.org/x/sys v0.0.0-20191119195528-f068ffe820e4 // indirect
 	golang.org/x/text v0.3.2 // indirect
-	google.golang.org/genproto v0.0.0-20191108220845-16a3f7862a1a // indirect
+	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11 // indirect
 	google.golang.org/grpc v1.25.1
 )
--- parse.go
+++ parse.go
@@ -10,9 +10,8 @@ import (
 )
 
 type (
-	Nothing     struct{}
-	IntSet      map[int32]Nothing
-	ArrayIntSet []int32
+	Nothing struct{}
+	IntSet  map[int32]Nothing
 )
 
 var NothingV = Nothing{}
@@ -150,29 +149,4 @@ func Parse2(UpdateTime int64) {
 		v.RegistryUpdateTime = UpdateTime
 	}
 	DumpSnap.Content.Unlock()
-}
-
-func (a ArrayIntSet) Blank() bool { return len(a) == 0 }
-
-func (a ArrayIntSet) Add(v int32) ArrayIntSet {
-	for i := range a {
-		if a[i] == v {
-			return a
-		}
-	}
-	return append(a, v)
-}
-
-func (a ArrayIntSet) Del(v int32) ArrayIntSet {
-	idx := -1
-	for i := range a {
-		if a[i] == v {
-			idx = i
-			break
-		}
-	}
-	if idx == -1 {
-		return a
-	}
-	return append(a[:idx], a[idx+1:]...)
 }
