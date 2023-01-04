8821c6575bbdc809770e2788f9816379f71a4aa7
Phil Kulin <schors@gmail.com>
2023-01-04 19:10:41 +0200

    change: refactoring phase 6

--- parse_time.go
+++ parse_time.go
@@ -2,15 +2,29 @@ package main
 
 import "time"
 
+var locationMSK *time.Location
+
+func init() {
+	var err error
+
+	locationMSK, err = time.LoadLocation("Europe/Moscow")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func parseTime(s string) int64 {
 	if s == "" {
 		return 0
 	}
+
 	t, err := time.Parse(time.RFC3339, s)
 	if err != nil {
-		Error.Printf("Can't parse time: %s (%s)\n", err.Error(), s)
+		Error.Printf("Can't parse time: %s (%s)\n", err, s)
+
 		return 0
 	}
+
 	return t.Unix()
 }
 
@@ -20,10 +34,13 @@ func parseTime2(s string) int64 {
 	if s == "" {
 		return 0
 	}
-	t, err := time.Parse(parseIncludeTime, s)
+
+	t, err := time.ParseInLocation(parseIncludeTime, s, locationMSK)
 	if err != nil {
-		Error.Printf("Can't parse time: %s (%s)\n", err.Error(), s)
+		Error.Printf("Can't parse time: %s (%s)\n", err, s)
+
 		return 0
 	}
-	return t.Unix() - 3600*3
+
+	return t.Unix()
 }
