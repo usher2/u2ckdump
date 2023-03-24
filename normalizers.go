0de93afbcd4d5916549b2fbb187331fdeb3f0dd0
Phil Kulin <schors@gmail.com>
2023-03-25 01:31:37 +0200

    change: normilizers.go and add tests

--- normalizers.go
+++ normalizers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/url"
+	"regexp"
 	"strings"
 
 	"golang.org/x/net/idna"
@@ -9,46 +10,98 @@ import (
 	"github.com/usher2/u2ckdump/internal/logger"
 )
 
-// NormalizeDomain - normilize domain name.
+// NormalizeDomain takes a domain name string containing misprints and
+// attempts to construct the correct domain name. It trims unnecessary characters,
+// replaces common errors, and converts the domain to ASCII and lowercase.
+// If there is an error during the conversion to ASCII, it is ignored and the original
+// domain is returned instead.
 func NormalizeDomain(domain string) string {
-	if c := strings.IndexByte(domain, '/'); c >= 0 {
-		domain = domain[:c]
-	}
+	// Remove the protocol or its misspellings, if present
+	domain = removeMisspelledProtocol(domain)
 
-	if c := strings.IndexByte(domain, '\\'); c >= 0 {
-		domain = domain[:c]
-	}
+	// Remove any content after the first '/' or '\' character.
+	domain = trimAfterChar(domain, '/')
+	domain = trimAfterChar(domain, '\\')
 
+	// Replace common misprints and unnecessary characters.
 	domain = strings.Replace(domain, ",", ".", -1)
 	domain = strings.Replace(domain, " ", "", -1)
+	//	domain = strings.Replace(domain, "_", "-", -1)      // Replace underscore with hyphen.
+	//	domain = strings.Replace(domain, "wwww", "www", -1) // Fix common "wwww" misprint.
 	domain = strings.TrimPrefix(domain, "*.")
 	domain = strings.TrimSuffix(domain, ".")
-	domain, _ = idna.ToASCII(domain)
-	domain = strings.ToLower(domain)
 
-	return domain
+	// Convert domain to ASCII and ignore any errors.
+	asciiDomain, _ := idna.ToASCII(domain)
+
+	// Convert domain to lowercase.
+	lowerDomain := strings.ToLower(asciiDomain)
+
+	return lowerDomain
 }
 
-// NormalizeURL - normilize URL.
+// NormalizeURL takes a URL string containing misprints and
+// attempts to construct the correct URL. It fixes common misprints,
+// normalizes the domain using the NormalizeDomain function, and
+// removes any URL fragments.
 func NormalizeURL(u string) string {
+	// Fix the misspelled protocol, if present
+	u = replaceMisspelledProtocol(u)
+
+	// Replace backslashes with forward slashes.
 	u = strings.Replace(u, "\\", "/", -1)
 
+	// Parse the URL.
 	nurl, err := url.Parse(u)
 	if err != nil {
+		// Log the error and return the original URL if parsing fails.
 		logger.Error.Printf("URL parse error: %s\n", err)
 
 		return u
 	}
 
+	// Normalize the domain.
 	domain := nurl.Hostname()
 	port := nurl.Port()
-
 	nurl.Host = NormalizeDomain(domain)
+
+	// Add the port back to the normalized domain, if present.
 	if port != "" {
 		nurl.Host = nurl.Host + ":" + port
 	}
 
+	// Remove any URL fragments.
 	nurl.Fragment = ""
 
+	// Return the normalized URL.
 	return nurl.String()
+}
+
+// protocolPattern - regexp for remove misspelled protocol.
+var protocolPattern = regexp.MustCompile(`^(https?):?[/\\]*|^(http?):?[/\\]*|^//`)
+
+// removeMisspelledProtocol removes common misspellings of the "http://" or "https://" prefix
+// from the input domain string if it is present.
+func removeMisspelledProtocol(s string) string {
+	return protocolPattern.ReplaceAllString(s, "")
+}
+
+// replaceMisspelledProtocol replaces common misspellings of the "http://" or "https://" prefix
+// in the input URL string with the correct protocol.
+func replaceMisspelledProtocol(s string) string {
+	return protocolPattern.ReplaceAllStringFunc(s, func(match string) string {
+		if strings.Contains(match, "https") {
+			return "https://"
+		}
+		return "http://"
+	})
+}
+
+// trimAfterChar trims the input string after the first occurrence of the specified character.
+// If the character is not found, the original string is returned.
+func trimAfterChar(s string, char byte) string {
+	if idx := strings.IndexByte(s, char); idx >= 0 {
+		return s[:idx]
+	}
+	return s
 }
---
+++ normalizers_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestNormalizeDomain tests the NormalizeDomain function.
+func TestNormalizeDomain(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"example.com", "example.com"},
+		{"http://example.com", "example.com"},
+		{"http:\\example.com", "example.com"},
+		{"http//example.com", "example.com"},
+		{"//example.com", "example.com"},
+		{"http:/example.com", "example.com"},
+		{"http/exmaple.com", "exmaple.com"},
+		{"http://example.com/test", "example.com"},
+		{"Example,com", "example.com"},
+		{"example . com", "example.com"},
+		{"*.example.com", "example.com"},
+		{"example.com.", "example.com"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			result := NormalizeDomain(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %q, got %q", tc.expected, result)
+			}
+		})
+	}
+}
+
+// TestNormalizeURL tests the NormalizeURL function.
+func TestNormalizeURL(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"http://example.com", "http://example.com"},
+		{"https://example.com", "https://example.com"},
+		{"http:\\\\example.com", "http://example.com"},
+		{"http:/example.com", "http://example.com"},
+		{"http:example.com", "http://example.com"},
+		{"http://Example,com", "http://example.com"},
+		{"http://example.com/test#fragment", "http://example.com/test"},
+		{"https://example.com:8080", "https://example.com:8080"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.input, func(t *testing.T) {
+			result := NormalizeURL(tc.input)
+			if result != tc.expected {
+				t.Errorf("expected %q, got %q", tc.expected, result)
+			}
+		})
+	}
+}
